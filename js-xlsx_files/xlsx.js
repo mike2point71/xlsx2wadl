ae804a882148b04d206160847845a1cebece769f
Mike Wright <mw@sb121.(none)>
2013-05-19 15:23:54 -0700

    Looks like decode_range is doing the right stuff but sheet['\!ref'] is wrong.

--- js-xlsx_files/xlsx.js
+++ js-xlsx_files/xlsx.js
@@ -965,7 +965,11 @@ function decode_col(c) { var d = 0, i = 0; for(; i !== c.length; ++i) d = 26*d +
 function decode_row(rowstr) { return Number(rowstr) - 1; }
 function split_cell(cstr) { return cstr.replace(/(\$?[A-Z]*)(\$?[0-9]*)/,"$1,$2").split(","); }
 function decode_cell(cstr) { var splt = split_cell(cstr); return { c:decode_col(splt[0]), r:decode_row(splt[1]) }; }
-function decode_range(range) { var x =range.split(":").map(decode_cell); return {s:x[0],e:x[x.length-1]}; }
+function decode_range(range) {
+	alert("passedRange: " + JSON.stringify(range));
+	var x =range.split(":").map(decode_cell);
+	return {s:x[0],e:x[x.length-1]};
+}
 function encode_range(range) { return encode_cell(range.s) + ":" + encode_cell(range.e); }
 /**
  * Convert a sheet into an array of objects where the column headers are keys.
