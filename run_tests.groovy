173980ef9c394c80c84fa1ae7b9d440a96af3d94
Mike Wright <mw@sb121.(none)>
2013-05-19 01:00:51 -0700

    Deleting useless lines

--- run_tests.groovy
+++ run_tests.groovy
@@ -9,32 +9,5 @@ println absPath
 Browser.drive {
   go "file://" +  absPath + "/mk-xlsx1.html"
   $("form").fileUpload = absPath + "/testing_resources/xlsxtest1.xlsx"
-  fileInputBox = $("input")
-  fileInputBox.each{
-    println it
-    it.each{
-      println it
-    }
-  }
-  fileInputBox = absPath + "/testing_resources/xlsxtest1.xlsx"
 
-  // make sure we actually got to the page
-  //assert title == "Google"
-
-  // enter wikipedia into the search field
-  //$("input", name: "q").value("wikipedia")
-
-  // wait for the change to results page to happen
-  // (google updates the page dynamically without a new request)
-  //waitFor { title.endsWith("Google Search") }
-
-  // is the first link to wikipedia?
-  //firstLink = $("li.g", 0).find("a.l")
-  //assert firstLink.text() == "Wikipedia"
-
-  // click the link 
-  //firstLink.click()
-
-  // wait for Google's javascript to redirect to Wikipedia
-  //waitFor { title.contains("Wikipedia") }
 }
