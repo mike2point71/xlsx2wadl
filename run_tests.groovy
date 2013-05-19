34d3ed4f8b4421b3c5a4321a3f10cd424af2ef4d
Mike Wright <mw@sb121.(none)>
2013-05-19 01:00:22 -0700

    Test is kinda working but not asserting anything

--- run_tests.groovy
+++ run_tests.groovy
@@ -4,28 +4,37 @@
     @Grab("org.seleniumhq.selenium:selenium-support:2.32.0")
 ])
 import geb.Browser
-absPath = new File(".").absolutePath
+absPath = new File("").absolutePath
 println absPath
 Browser.drive {
-    go "file://" +  absPath + "/mk-xlsx1.html"
- 
-    // make sure we actually got to the page
-    //assert title == "Google"
- 
-    // enter wikipedia into the search field
-    //$("input", name: "q").value("wikipedia")
- 
-    // wait for the change to results page to happen
-    // (google updates the page dynamically without a new request)
-    //waitFor { title.endsWith("Google Search") }
- 
-    // is the first link to wikipedia?
-    //firstLink = $("li.g", 0).find("a.l")
-    //assert firstLink.text() == "Wikipedia"
- 
-    // click the link 
-    //firstLink.click()
- 
-    // wait for Google's javascript to redirect to Wikipedia
-    //waitFor { title.contains("Wikipedia") }
+  go "file://" +  absPath + "/mk-xlsx1.html"
+  $("form").fileUpload = absPath + "/testing_resources/xlsxtest1.xlsx"
+  fileInputBox = $("input")
+  fileInputBox.each{
+    println it
+    it.each{
+      println it
+    }
+  }
+  fileInputBox = absPath + "/testing_resources/xlsxtest1.xlsx"
+
+  // make sure we actually got to the page
+  //assert title == "Google"
+
+  // enter wikipedia into the search field
+  //$("input", name: "q").value("wikipedia")
+
+  // wait for the change to results page to happen
+  // (google updates the page dynamically without a new request)
+  //waitFor { title.endsWith("Google Search") }
+
+  // is the first link to wikipedia?
+  //firstLink = $("li.g", 0).find("a.l")
+  //assert firstLink.text() == "Wikipedia"
+
+  // click the link 
+  //firstLink.click()
+
+  // wait for Google's javascript to redirect to Wikipedia
+  //waitFor { title.contains("Wikipedia") }
 }
